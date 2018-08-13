c05d8dc0ff94476e043cc637fdf90accfb7069da
Mikalai Biadrytski <Mikalai_Biadrytski@epam.com>
2018-08-13 15:55:32 +0300

    some packages files

---
+++ 02_packages/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nbiadrytski/udemyGoTutorial/02_packages/icomefromalaska"
+	"github.com/nbiadrytski/udemyGoTutorial/02_packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
