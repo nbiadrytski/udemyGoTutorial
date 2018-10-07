9eae5d314eb5daf533ad87fd0233ceb68af18e85
Nickolay Biadrytski <nickbiadrytski@gmail.com>
2018-10-07 20:07:47 +0200

    more interfaces demos

--- 21_interfaces/exercise1_sorting/main.go
+++ 21_interfaces/exercise1_sorting/main.go
@@ -6,49 +6,48 @@ import (
 )
 
 type people []string
-type digits []int
 
 func (p people) Len() int {
 	return len(p)
-}
-
-func (d digits) Len() int {
-	return len(d)
-}
-
-func (p people) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
-
-func (d digits) Swap(i, j int) {
-	d[i], d[j] = d[j], d[i]
 }
 
 func (p people) Less(i, j int) bool {
 	return p[i] < p[j]
 }
 
-func (d digits) Less(i, j int) bool {
-	return d[i] < d[j]
+func (p people) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
 }
 
 func main() {
+	// sorting user-defined type
 	studyGroup := people{"Zeno", "Joe", "Al", "Jenny"}
-	fmt.Println("before sorting", studyGroup)
+	fmt.Println("before sorting (user-defined type)", studyGroup)
 
 	sort.Sort(studyGroup)
-	fmt.Println("after sorting", studyGroup)
+	fmt.Println("after sorting (user-defined type)", studyGroup)
 
 	sort.Sort(sort.Reverse(studyGroup))
-	fmt.Println("after reverse", studyGroup)
+	fmt.Println("after reverse (user-defined type)", studyGroup)
 
-	n := []int{2, 4, 3, 6}
-	fmt.Println("before sorting", n)
+	// sorting slice of strings
+	s := []string{"Zeno", "Joe", "Al", "Jenny"}
+	fmt.Println("before sorting (slice of strings)", s)
+	// sort.Strings(s)
+	// another way; turns s into StringSlice type which has Sort(), Swap(), etc. methods
+	sort.StringSlice(s).Sort()
+	fmt.Println("after sorting (slice of strings)", s)
+	sort.Sort(sort.Reverse(sort.StringSlice(s)))
+	fmt.Println("after reverse (slice of strings)", s)
 
-	sort.Sort(digits(n))
-	fmt.Println("after sorting", n)
-
-	sort.Sort(sort.Reverse(digits(n)))
-	fmt.Println("after reverse", n)
+	// sorting slice of ints
+	n := []int{5, 2, 6, 3, 1, 4}
+	fmt.Println("before sorting (slice of ints)", n)
+	//sort.Ints(n)
+	// another way; turns s into IntSlice type which has Sort(), Swap(), etc. methods
+	sort.IntSlice(n).Sort()
+	fmt.Println("after sorting (slice of ints)", n)
+	sort.Sort(sort.Reverse(sort.IntSlice(n)))
+	fmt.Println("after reverse (slice of ints)", n)
 
 }
