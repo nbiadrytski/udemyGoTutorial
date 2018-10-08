ebb7a3da6a37cafcac1ce1f47a1e6d4b86ca0a8e
Nickolay Biadrytski <nickbiadrytski@gmail.com>
2018-10-08 18:00:23 +0300

    added more interface demos

---
+++ 21_interfaces/03_empty-interfaces/04_slice-of-any-type/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type animal struct {
+	sound string
+}
+
+type dog struct {
+	animal
+	friendly bool
+}
+
+type cat struct {
+	animal
+	annoying bool
+}
+
+func main() {
+	fido := dog{animal{"woof"}, true}
+	fifi := cat{animal{"meow"}, true}
+	shadow := dog{animal{"woof"}, true}
+	// slice of interface which is actually slice of any type
+	critters := []interface{}{fido, fifi, shadow}
+	fmt.Println(critters)
+}
