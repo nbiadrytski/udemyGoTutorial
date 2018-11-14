04d7350b5837b89a6d1335a7f49b9e8a6c3c31fb
Nickolay Biadrytski <nickbiadrytski@gmail.com>
2018-11-14 11:18:38 +0300

    added more fan in out stuff

---
+++ 22_go-routines/13_channels-fan-out-fan-in/02_square-output/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	in := gen(2, 3)
+
+	// FAN OUT
+	// Distribute the sq work across two goroutines that both read from in.
+	// multiple func reading frm one channel
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// FAN IN
+	// Consume the merged output from multiple channels.
+	for n := range merge(c1, c2) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
+}
+
+// generate some numbers and write them to channel
+func gen(nums ...int) chan int {
+	fmt.Printf("TYPE OF NUMS %T\n", nums) // just FYI
+
+	out := make(chan int)
+	go func() {
+		for _, v := range nums {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+// takes in a channel of int, calculates squares of ints and
+// writes the results to channel
+func sq(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for v := range in {
+			out <- v * v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...chan int) chan int {
+	fmt.Printf("TYPE OF CS: %T\n", cs) // just FYI
+
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go func(ch chan int) {
+			for n := range ch {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
